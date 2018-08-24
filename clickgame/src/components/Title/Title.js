59db727b754185ee5e3744e5e9f64fd741572745
Zattelin <37785003+Zattelin@users.noreply.github.com>
2018-08-23 23:18:39 -0400

    Updates

--- clickgame/src/components/Title/Title.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Title.css";
-
-// display for game objectives
-const Title = props => <h1 className="title brand animated pulse">{props.children}</h1>;
-
-export default Title;
